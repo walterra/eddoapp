56e3c605953469392bded2b360c0600295e7814a
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-09-06 13:10:36 +0200

    query via design docs

--- src/components/todo_board.tsx
+++ src/components/todo_board.tsx
@@ -1,13 +1,15 @@
-import { useEffect, useMemo, useState, type FC } from 'react';
+import { useEffect, useMemo, useRef, useState, type FC } from 'react';
 import { group } from 'd3-array';
 import { add, format, getISOWeek, startOfWeek, endOfWeek } from 'date-fns';
+import { uniqBy, isEqual } from 'lodash-es';
 
 import { CONTEXT_DEFAULT } from '../constants';
 import { usePouchDb } from '../pouch_db';
 
 import { isLatestVersion, migrateTodo } from '../api/versions/migrate';
+import { type Activity } from '../types/activity';
 import { type Todo } from '../types/todo';
-import { getFormattedDurationForTodos } from '../utils/get_formatted_duration';
+import { getFormattedDurationForActivities } from '../utils/get_formatted_duration';
 
 import { FormattedMessage } from './formatted_message';
 import { TodoListElement } from './todo_list_element';
@@ -16,6 +18,36 @@ interface TodoBoardProps {
   currentDate: Date;
 }
 
+const designDocId = '_design/todos';
+const designDocByActiveView = 'todos/byActive';
+const designDocByDueDateView = 'todos/byDueDate';
+const designDocByTimeTrackingActiveView = 'todos/byTimeTrackingActive';
+const designDocViews = {
+  byActive: {
+    map: function (doc) {
+      if (doc.active) {
+        Object.entries(doc.active).forEach(([from, to]) => {
+          emit(from, { doc, from, id: doc._id, to });
+        });
+      }
+    }.toString(),
+  },
+  byDueDate: {
+    map: function (doc) {
+      if (doc.due) {
+        emit(doc.due, doc);
+      }
+    }.toString(),
+  },
+  byTimeTrackingActive: {
+    map: function (doc) {
+      Object.entries((doc as Todo).active).forEach((d) => {
+        emit(d[1], {});
+      });
+    }.toString(),
+  },
+};
+
 export const TodoBoard: FC<TodoBoardProps> = ({ currentDate }) => {
   const db = usePouchDb();
   const [timeTrackingActive, setTimeTrackingActive] = useState<string[]>([
@@ -23,6 +55,13 @@ export const TodoBoard: FC<TodoBoardProps> = ({ currentDate }) => {
   ]);
   const [outdatedTodos, setOutdatedTodos] = useState<unknown[]>([]);
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  // avoid multiple fetches
+  const isFetching = useRef(false);
+  const shouldFetch = useRef(true);
+  // check integrity, e.g. if design docs are present
+  const [isInitialized, setIsInitialized] = useState(false);
 
   const currentCalendarWeek = getISOWeek(currentDate);
   // TODO The 'add' is a CEST quick fix
@@ -36,6 +75,52 @@ export const TodoBoard: FC<TodoBoardProps> = ({ currentDate }) => {
   });
 
   useEffect(() => {
+    if (isInitialized) return;
+
+    // async iife
+    (async () => {
+      let designDoc;
+
+      try {
+        // Attempt to get the design document from the database
+        designDoc = await db.get(designDocId);
+
+        // Check if design doc has all views
+        if (!isEqual(designDoc.views, designDocViews)) {
+          throw new Error('Design document is missing views.');
+        }
+
+        // You can then proceed with your logic, e.g., update or query it
+        setIsInitialized(true);
+      } catch (err) {
+        // If an error occurs, it means the design document does not exist
+        if (err.status === 404) {
+          // Save the design document to your database
+          await db.put({
+            _id: '_design/todos',
+            views: designDocViews,
+          });
+
+          setIsInitialized(true);
+        } else if (err.message === 'Design document is missing views.') {
+          // Save the design document to your database
+          await db.put({
+            ...designDoc,
+            views: designDocViews,
+          });
+
+          setIsInitialized(true);
+        } else {
+          // Handle other errors
+          console.error('Error retrieving design document:', err);
+        }
+      }
+    })();
+  }, [isInitialized]);
+
+  useEffect(() => {
+    if (!isInitialized) return;
+
     const listener = db
       .changes({
         live: true,
@@ -51,21 +136,13 @@ export const TodoBoard: FC<TodoBoardProps> = ({ currentDate }) => {
     return () => {
       listener.cancel();
     };
-  }, [currentDate]);
+  }, [currentDate, isInitialized]);
 
   async function fetchTimeTrackingActive() {
     try {
-      // TODO query via design doc
-      const resp = await db.query(
-        (doc, emit) => {
-          if (emit) {
-            Object.entries((doc as Todo).active).forEach((d) => {
-              emit(d[1], {});
-            });
-          }
-        },
-        { key: null },
-      );
+      const resp = await db.query(designDocByTimeTrackingActiveView, {
+        key: null,
+      });
       setTimeTrackingActive(resp.rows.map((d) => d.id));
     } catch (e) {
       console.error('not able to fetch active todos', e);
@@ -73,74 +150,130 @@ export const TodoBoard: FC<TodoBoardProps> = ({ currentDate }) => {
   }
 
   async function fetchTodos() {
-    try {
-      // TODO query via design doc
-      const newTodos = await db.query(
-        (doc, emit) => {
-          if (emit) {
-            emit((doc as Todo).due, doc);
-          }
-        },
-        {
-          descending: false,
-          endkey: currentEndOfWeek.toISOString(),
-          include_docs: false,
-          startkey: currentStartOfWeek.toISOString(),
-        },
-      );
+    if (isFetching.current) {
+      shouldFetch.current = true;
+      return;
+    }
 
+    isFetching.current = true;
+
+    try {
+      console.time('fetchTodos');
+      const newTodos = await db.query(designDocByDueDateView, {
+        descending: false,
+        endkey: currentEndOfWeek.toISOString(),
+        include_docs: false,
+        startkey: currentStartOfWeek.toISOString(),
+      });
+      console.timeEnd('fetchTodos');
+
+      console.time('fetchActivities');
+      const newActivities = await db.query(designDocByActiveView, {
+        descending: false,
+        endkey: currentEndOfWeek.toISOString(),
+        include_docs: false,
+        startkey: currentStartOfWeek.toISOString(),
+      });
+      console.timeEnd('fetchActivities');
+
+      console.time('setOutdatedTodos');
       setOutdatedTodos(
         newTodos?.rows
           .filter((d) => !isLatestVersion(d.value))
           .map((d) => d.value),
       );
+      console.timeEnd('setOutdatedTodos');
 
+      console.time('setTodos');
       setTodos(
         (newTodos?.rows
           .filter((d) => isLatestVersion(d.value))
           .map((d) => d.value) ?? []) as Todo[],
       );
+      console.timeEnd('setTodos');
+
+      console.time('setActivities');
+      setActivities(newActivities.rows.map((d) => d.value));
+      console.timeEnd('setActivities');
     } catch (err) {
       console.error(err);
+    } finally {
+      isFetching.current = false;
+      if (shouldFetch.current) {
+        shouldFetch.current = false;
+        fetchTodos();
+      }
     }
   }
 
   useEffect(() => {
+    if (!isInitialized) return;
+
     fetchTodos();
     fetchTimeTrackingActive();
-  }, [currentCalendarWeek]);
+  }, [currentCalendarWeek, isInitialized]);
 
   useEffect(() => {
+    if (!isInitialized) return;
+
+    // disabled for now
+    return;
+
     db.bulkDocs(outdatedTodos.map((d) => migrateTodo(d)));
-  }, [outdatedTodos]);
+  }, [outdatedTodos, isInitialized]);
+
+  const filteredActivities = activities.filter((a) => {
+    // TODO The 'split' is a CEST quick fix
+    return !todos.some(
+      (t) => a.id === t._id && a.from.split('T')[0] === t.due.split('T')[0],
+    );
+  });
 
   const groupedByContextByDate = useMemo(() => {
     const grouped = Array.from(
       group(
-        todos,
-        (d) => d.context ?? CONTEXT_DEFAULT,
-        // TODO The 'split' is a CEST quick fix
-        (d) => d.due.split('T')[0], // format(new Date(d.due), 'yyyy-MM-dd'),
+        [...todos, ...filteredActivities],
+        (d) => {
+          if (isLatestVersion(d)) {
+            return d.context ?? CONTEXT_DEFAULT;
+          } else {
+            return d.doc.context ?? CONTEXT_DEFAULT;
+          }
+        },
+        (d) => {
+          // TODO The 'split' is a CEST quick fix
+          if (isLatestVersion(d)) {
+            return d.due.split('T')[0]; // format(new Date(d.due), 'yyyy-MM-dd'),
+          } else {
+            return d.from.split('T')[0];
+          }
+        },
       ),
     );
     grouped.sort((a, b) => ('' + a[0]).localeCompare(b[0]));
     return grouped;
   }, [todos]);
 
-  const durationByContext = useMemo(
-    () =>
-      Object.fromEntries(
-        Array.from(group(todos, (d) => d.context ?? CONTEXT_DEFAULT)).map(
-          (d) => {
-            const context = d[0];
-            const entries = d[1];
+  const durationByContext = useMemo(() => {
+    return Object.fromEntries(
+      Array.from(
+        group(activities, (d) => d.doc.context ?? CONTEXT_DEFAULT),
+      ).map((d) => [d[0], getFormattedDurationForActivities(d[1])]),
+    );
+  }, [activities]);
 
-            return [context, getFormattedDurationForTodos(entries)];
-          },
-        ),
+  const durationByContextByDate = useMemo(() => {
+    const grouped = Array.from(
+      group(
+        activities,
+        (d) => d.doc.context ?? CONTEXT_DEFAULT,
+        // TODO The 'split' is a CEST quick fix
+        (d) => d.from.split('T')[0], // format(new Date(d.from), 'yyyy-MM-dd'),
       ),
-    [todos],
-  );
+    );
+    grouped.sort((a, b) => ('' + a[0]).localeCompare(b[0]));
+    return grouped;
+  }, [activities]);
 
   const dataStr =
     'data:text/json;charset=utf-8,' +
@@ -155,7 +288,23 @@ export const TodoBoard: FC<TodoBoardProps> = ({ currentDate }) => {
               <div className="mb-6 flex items-start justify-start space-x-4 px-4">
                 {groupedByContextByDate.map(([context, contextTodos]) => {
                   const todosByDate = Array.from(contextTodos);
+
                   todosByDate.sort((a, b) => ('' + a[0]).localeCompare(b[0]));
+
+                  const activitiesByMapDate = durationByContextByDate.find(
+                    (d) => d[0] === context,
+                  );
+
+                  const activityDurationByDate = activitiesByMapDate
+                    ? Array.from(activitiesByMapDate[1]).map((d) => [
+                        d[0],
+                        getFormattedDurationForActivities(d[1]),
+                      ])
+                    : [];
+                  activityDurationByDate.sort((a, b) =>
+                    ('' + a[0]).localeCompare(b[0]),
+                  );
+
                   return (
                     <div className="eddo-w-kanban" key={context}>
                       <div className="py-4 text-base font-semibold text-gray-900 dark:text-gray-300">
@@ -173,10 +322,21 @@ export const TodoBoard: FC<TodoBoardProps> = ({ currentDate }) => {
                         className="eddo-w-kanban mb-4 space-y-4"
                         id="kanban-list-1"
                       >
-                        {todosByDate.map(([todoDate, todosForDate]) => {
-                          todosForDate.sort((a, b) =>
-                            ('' + a.title).localeCompare(b.title),
-                          );
+                        {todosByDate.map(([todoDate, allTodosForDate]) => {
+                          const todosForDate = uniqBy(allTodosForDate, (d) => {
+                            return isLatestVersion(d) ? d._id : d.id;
+                          });
+
+                          todosForDate.sort((a, b) => {
+                            const aTitle = isLatestVersion(a)
+                              ? a.title
+                              : a.doc.title;
+                            const bTitle = isLatestVersion(b)
+                              ? b.title
+                              : b.doc.title;
+
+                            return ('' + aTitle).localeCompare(bTitle);
+                          });
 
                           let displayDate = '';
 
@@ -189,26 +349,44 @@ export const TodoBoard: FC<TodoBoardProps> = ({ currentDate }) => {
                             displayDate = format(new Date(), 'yyyy-MM-dd');
                           }
 
+                          const activityDurationItem =
+                            activityDurationByDate.find(
+                              (d) => d[0] === displayDate,
+                            );
+
+                          const durationForDate = activityDurationItem
+                            ? activityDurationItem[1]
+                            : '';
+
                           return (
                             <div key={`${context}_${todoDate}`}>
                               <div className="flex items-center justify-between">
                                 <div className="mx-1">{displayDate}</div>
                                 <div className="mx-1 text-xs text-gray-400">
-                                  {getFormattedDurationForTodos(todosForDate)}
+                                  {durationForDate}
                                 </div>
                               </div>
-                              {todosForDate.map((todo) => (
-                                <TodoListElement
-                                  active={timeTrackingActive.some(
-                                    (d) => d === todo._id,
-                                  )}
-                                  key={todo._id}
-                                  timeTrackingActive={
-                                    timeTrackingActive.length > 0
-                                  }
-                                  todo={todo}
-                                />
-                              ))}
+                              {todosForDate.map((todoOrActivity) => {
+                                const todo = isLatestVersion(todoOrActivity)
+                                  ? todoOrActivity
+                                  : todoOrActivity.doc;
+                                return (
+                                  <TodoListElement
+                                    active={timeTrackingActive.some(
+                                      (d) => d === todo._id,
+                                    )}
+                                    activeDate={displayDate}
+                                    activityOnly={
+                                      !isLatestVersion(todoOrActivity)
+                                    }
+                                    key={todo._id}
+                                    timeTrackingActive={
+                                      timeTrackingActive.length > 0
+                                    }
+                                    todo={todo}
+                                  />
+                                );
+                              })}
                             </div>
                           );
                         })}
