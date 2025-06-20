05e544713cdc6477d2d94774fdd48d87f6c322cb
Walter M. Rafelsberger <walter@rafelsberger.at>
2025-06-20 08:44:30 +0200

    feat: extract shared types and utilities to packages/shared

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,17 @@
+// Types
+export * from './types/todo';
+export * from './types/activity';
+
+// API versions
+export * from './api/versions/todo_alpha1';
+export * from './api/versions/todo_alpha2';
+export * from './api/versions/todo_alpha3';
+export * from './api/versions/migrate';
+
+// Utils
+export * from './utils/get_active_duration';
+export * from './utils/get_active_record_for_activities';
+export * from './utils/get_active_record_for_todos';
+export * from './utils/get_formatted_duration';
+export * from './utils/get_repeat_todo';
+export * from './utils/shuffle';
