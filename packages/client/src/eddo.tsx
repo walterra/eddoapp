9c30bc8b793e30959dbcaed2cc8e66c72eb3ebbc
Walter M. Rafelsberger <walter@rafelsberger.at>
2025-07-02 15:53:33 +0200

    refactor: centralize PouchDB change listeners and fix import ordering (#65)
    * refactor: centralize PouchDB change listeners to fix max listeners warning
    
    * refactor: reorder imports and use sequence numbers for database changes

--- packages/client/src/eddo.tsx
+++ packages/client/src/eddo.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { AddTodo } from './components/add_todo';
 import { PageWrapper } from './components/page_wrapper';
 import { TodoBoard } from './components/todo_board';
+import { DatabaseChangesProvider } from './hooks/use_database_changes';
 import { useDatabaseHealth } from './hooks/use_database_health';
 import { useSyncDev } from './hooks/use_sync_dev';
 import { PouchDbContext, pouchDbContextValue } from './pouch_db';
@@ -28,17 +29,19 @@ export function Eddo() {
 
   return (
     <PouchDbContext.Provider value={pouchDbContextValue}>
-      <DevSync />
-      <HealthMonitor />
-      <PageWrapper>
-        <AddTodo
-          currentDate={currentDate}
-          selectedTags={selectedTags}
-          setCurrentDate={setCurrentDate}
-          setSelectedTags={setSelectedTags}
-        />
-        <TodoBoard currentDate={currentDate} selectedTags={selectedTags} />
-      </PageWrapper>
+      <DatabaseChangesProvider>
+        <DevSync />
+        <HealthMonitor />
+        <PageWrapper>
+          <AddTodo
+            currentDate={currentDate}
+            selectedTags={selectedTags}
+            setCurrentDate={setCurrentDate}
+            setSelectedTags={setSelectedTags}
+          />
+          <TodoBoard currentDate={currentDate} selectedTags={selectedTags} />
+        </PageWrapper>
+      </DatabaseChangesProvider>
     </PouchDbContext.Provider>
   );
 }
