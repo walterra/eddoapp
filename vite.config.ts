eec633d9bde4f333abb4d31515d74f2069ccb308
Walter M. Rafelsberger <walter@rafelsberger.at>
2025-07-17 00:25:43 +0200

    refactor: restructure packages and add VS Code configuration with Flowbite React setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), flowbiteReact()],
   root: './packages/web-client',
   build: {
     outDir: '../../dist',
